ec97493db58d54364aa9553c61d42c1ab8a436cf
Sergiu-19 <42917431+Sergiu-19@users.noreply.github.com>
2019-06-17 00:28:36 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var _ = require("lodash");
+
+var array = [1, 2, 3, 4, 5, 6, 7];
+console.log("answer:", _.without(array, 3));
+
+const css = document.querySelector("h3");
+const color1 = document.querySelector(".color1");
+const color2 = document.querySelector(".color2");
+const body = document.getElementById("gradient");
+
+function setGradient() {
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  css.textContent = body.style.background + "+";
+}
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
