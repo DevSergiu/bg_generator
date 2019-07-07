9e562c92fef9c9ac49d53786b23b8dd8431c8a18
Sergiu-19 <42917431+Sergiu-19@users.noreply.github.com>
2019-07-07 09:50:26 +0300

    mini fix

--- script.js
+++ script.js
@@ -1,15 +1,14 @@
-var array = [1, 2, 3, 4, 5, 6, 7];
-console.log("answer:", _.without(array, 3));
-
 const css = document.querySelector("h3");
 const color1 = document.querySelector(".color1");
 const color2 = document.querySelector(".color2");
 const body = document.getElementById("gradient");
 
 function setGradient() {
-  body.style.background =
-    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-  css.textContent = body.style.background + "+";
+  body.style.background = `linear-gradient(to right,
+    ${color1.value},${color2.value})`;
+  css.textContent = `Linear-gradiend, to right: ${color1.value}, ${
+    color2.value
+  }`;
 }
 color1.addEventListener("input", setGradient);
 
